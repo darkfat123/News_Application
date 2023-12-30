2d58aa43fd15d28c7175dc503064baf0661d7c20
darkfat123 <supakorn.yookack@gmail.com>
2023-12-30 09:29:06 +0700

    add appbar

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text("หน้าหลัก"), Icon(Icons.menu_rounded)],
+        ),
+      ),
+      body: Column(children: []),
+    );
+  }
+}
